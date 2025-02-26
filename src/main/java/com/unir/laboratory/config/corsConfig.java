9a6d4f3c8c375c756f2fad794340ea4a85099930
deibiiz <davidm.bahillo@gmail.com>
2025-02-26 20:54:24 +0100

    integración api

---
+++ src/main/java/com/unir/laboratory/config/corsConfig.java
@@ -0,0 +1,24 @@
+package com.unir.laboratory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas las rutas
+                        .allowedOrigins("http://localhost:3001") // Permitir solo el frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permitir todos los headers
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
